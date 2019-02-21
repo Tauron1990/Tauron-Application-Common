f115a5154f5aa7b59bea4ad1e6656f3abad40e69
Alexander Bachmaier <Tauron.ab@gmail.com>
2019-02-21 09:29:46 +0100

    Commit

--- Tauron.Application.Common/Application/Ioc/BuildUp/Strategy/CompilationUnit.cs
+++ Tauron.Application.Common/Application/Ioc/BuildUp/Strategy/CompilationUnit.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using JetBrains.Annotations;
 
@@ -7,6 +8,44 @@ namespace Tauron.Application.Ioc.BuildUp.Strategy
     [PublicAPI]
     public sealed class CompilationUnit
     {
+        private class ConstCacheKey : IEquatable<ConstCacheKey>
+        {
+            public object Element { get; }
+
+            public Type Type { get; }
+
+            public ConstCacheKey(object element, Type type)
+            {
+                Element = element;
+                Type = type;
+            }
+
+            public bool Equals(ConstCacheKey other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return Equals(Element, other.Element) && Equals(Type, other.Type);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != GetType()) return false;
+                return Equals((ConstCacheKey) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((Element != null ? Element.GetHashCode() : 0) * 397) ^ (Type != null ? Type.GetHashCode() : 0);
+                }
+            }
+        }
+
+        private Dictionary<ConstCacheKey, Expression> _constDictionary = new Dictionary<ConstCacheKey, Expression>();
+
         public class VariableNamerImpl
         {
             private int _currentVariable;
@@ -30,11 +69,11 @@ namespace Tauron.Application.Ioc.BuildUp.Strategy
             }
         }
         
-        public string LifeTimeContext => VariableNamer.GetValiableName("LifeTimeContext");
+        public string LifeTimeContext => "LifeTimeContext";
 
-        public string Input => VariableNamer.GetValiableName("InputObject");
+        public string Input => "InputObject";
 
-        public string TargetName => VariableNamer.GetValiableName(nameof(TargetName));
+        public string TargetName => nameof(TargetName);
 
         public VariableNamerImpl VariableNamer { get; private set; }
 
@@ -43,5 +82,43 @@ namespace Tauron.Application.Ioc.BuildUp.Strategy
         public List<ParameterExpression> ParameterExpressions { get; } = new List<ParameterExpression>();
 
         public Dictionary<string, ParameterExpression> Variables { get; } = new Dictionary<string, ParameterExpression>();
+
+        public Expression GetConst(object obj, Type type)
+        {
+            if (obj == null && type == null)
+                type = typeof(object);
+            if (type == null)
+                type = obj.GetType();
+
+            var key = new ConstCacheKey(obj, type);
+
+            if (_constDictionary.TryGetValue(key, out var exp)) return exp;
+
+            exp = Expression.Constant(obj, type);
+            _constDictionary[key] = exp;
+
+            return exp;
+        }
+
+        public ParameterExpression GetRandomVariable(Type type)
+        {
+            string name = VariableNamer.GetRandomVariable();
+            var exp = Expression.Variable(type, name);
+
+            Variables[name] = exp;
+
+            return exp;
+        }
+
+        public ParameterExpression GetOrAddVariable(Type type, string name)
+        {
+            name = VariableNamer.GetValiableName(name);
+            if (Variables.TryGetValue(name, out var exp)) return exp;
+
+            exp = Expression.Variable(type, name);
+            Variables[name] = exp;
+
+            return exp;
+        }
     }
 }
