3cf4c21006bb0d93bfde17978eb99cfbf63e07a7
Alexander Bachmaier <Tauron.ab@gmail.com>
2019-11-04 12:20:14 +0100

    Commit

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-using Tauron.Application.Common.BaseLayer.BusinessLayer;
-using Tauron.Application.Common.BaseLayer.Core;
+using FastExpressionCompiler;
 
 namespace TestApp
 {
@@ -14,7 +14,21 @@ namespace TestApp
         
         static void Main(string[] args)
         {
-            
+            ParameterExpression variable = Expression.Variable(typeof(int), "TestValue");
+            var returnLabel = Expression.Label(typeof(int));
+            var returnExpression = Expression.Label(returnLabel, Expression.Constant(-1));
+
+            var block = Expression.Block(
+                new[] {variable}, 
+                Expression.Assign(variable, Expression.Constant(42)),
+                Expression.Call(typeof(Console).GetMethod(nameof(Console.WriteLine), new []{typeof(int)}), variable),
+                Expression.Block(new[] { variable },
+                    Expression.AddAssign(variable, Expression.Constant(8))),
+                Expression.Return(returnLabel, Expression.Constant(0)),
+                returnExpression);
+
+            Console.WriteLine(Expression.Lambda<Func<int>>(block).CompileFast()());
+
 
             Console.ReadKey();
         }
